55320442830b807c00f300e3678d9a335f8a2cad
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-10-31 19:30:09 +0100

    Update file paths in server folder, update CMakeLists.txt to avoid conflicts between server and clients

---
+++ server/Core/Core.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2020
+** RType
+** File description:
+** Created by Emilien
+*/
+
+#include "Core.hpp"
+
+void Core::run()
+{
+
+}
---
+++ server/Core/Core.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2020
+** RType
+** File description:
+** Created by Emilien
+*/
+
+#ifndef RTYPE_CORE_HPP
+#define RTYPE_CORE_HPP
+
+class Core {
+public:
+    void run();
+private:
+
+};
+
+#endif //RTYPE_CORE_HPP
