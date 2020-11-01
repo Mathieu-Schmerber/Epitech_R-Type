ccf34ed3186407ba4b589e70c5b0f19a3d50bdf1
Cyprien Ricque <cyprien.ricque@epitech.eu>
2020-11-02 00:02:40 +0100

    Comment each events

--- client/src/sfml/EventsSFML.cpp
+++ client/src/sfml/EventsSFML.cpp
@@ -14,54 +14,68 @@ void EventsSFML::update() {
     _scroll.movement = 0;
 
     while (_window->getWindow()->pollEvent(_event)) {
-        switch (_event.type) {
-            case sf::Event::Closed:
-                _window->getWindow()->close(); // FIXME On le laisse là ?
-                break;
+        manageEvents();
+    }
+}
 
-            case sf::Event::KeyPressed:
-                if (!isInVector(_keyPressed, static_cast<Engine::Inputs>(_event.key.code)))
-                    _keyPressed.push_back(static_cast<Engine::Inputs>(_event.key.code));
-                break;
+void EventsSFML::manageEvents()
+{
+    switch (_event.type) {
+        /** Window Closed **/
+        case sf::Event::Closed:
+            _window->getWindow()->close(); // FIXME On le laisse là ?
+            break;
 
-            case sf::Event::KeyReleased:
-                _keyReleased.push_back(static_cast<Engine::Inputs>(_event.key.code));
-                removeInVector(_keyPressed, static_cast<Engine::Inputs>(_event.key.code));
-                break;
+        /** Key Pressed **/
+        case sf::Event::KeyPressed:
+            if (!isInVector(_keyPressed, static_cast<Engine::Inputs>(_event.key.code)))
+                _keyPressed.push_back(static_cast<Engine::Inputs>(_event.key.code));
+            break;
 
-            case sf::Event::MouseButtonPressed:
-                if (!isInVector(_buttonsPressed, _mouseLink.at(_event.mouseButton.button)))
-                    _buttonsPressed.push_back(_mouseLink.at(_event.mouseButton.button));
-                _mousePos = {_event.mouseWheelScroll.x, _event.mouseWheelScroll.y};
-                break;
+        /** Key Released **/
+        case sf::Event::KeyReleased:
+            _keyReleased.push_back(static_cast<Engine::Inputs>(_event.key.code));
+            removeInVector(_keyPressed, static_cast<Engine::Inputs>(_event.key.code));
+            break;
 
-            case sf::Event::MouseButtonReleased:
-                _buttonsReleased.push_back(_mouseLink.at(_event.mouseButton.button));
-                removeInVector(_buttonsPressed, _mouseLink.at(_event.mouseButton.button));
-                _mousePos = {_event.mouseWheelScroll.x, _event.mouseWheelScroll.y};
-                break;
+        /** Mouse Button Pressed **/
+        case sf::Event::MouseButtonPressed:
+            if (!isInVector(_buttonsPressed, _mouseLink.at(_event.mouseButton.button)))
+                _buttonsPressed.push_back(_mouseLink.at(_event.mouseButton.button));
+            _mousePos = {_event.mouseWheelScroll.x, _event.mouseWheelScroll.y};
+            break;
 
-            case sf::Event::MouseWheelScrolled:
-                if (_event.mouseWheelScroll.wheel == sf::Mouse::VerticalWheel)
-                    _scroll.direction = Engine::Scroll::vertical;
-                else if (_event.mouseWheelScroll.wheel == sf::Mouse::HorizontalWheel)
-                    _scroll.direction = Engine::Scroll::horizontal;
-                else
-                    std::cerr << "wheel movement type: unknown" << std::endl;
-                _scroll.movement = _event.mouseWheelScroll.delta;
-                _mousePos = {_event.mouseWheelScroll.x, _event.mouseWheelScroll.y};
+        /** Mouse Button Released **/
+        case sf::Event::MouseButtonReleased:
+            _buttonsReleased.push_back(_mouseLink.at(_event.mouseButton.button));
+            removeInVector(_buttonsPressed, _mouseLink.at(_event.mouseButton.button));
+            _mousePos = {_event.mouseWheelScroll.x, _event.mouseWheelScroll.y};
+            break;
 
-            case sf::Event::MouseMoved:
-                _mousePos = {_event.mouseWheelScroll.x, _event.mouseWheelScroll.y};
+        /** Mouse Wheel Scrolled **/
+        case sf::Event::MouseWheelScrolled:
+            if (_event.mouseWheelScroll.wheel == sf::Mouse::VerticalWheel)
+                _scroll.direction = Engine::Scroll::vertical;
+            else if (_event.mouseWheelScroll.wheel == sf::Mouse::HorizontalWheel)
+                _scroll.direction = Engine::Scroll::horizontal;
+            else
+                std::cerr << "wheel movement type: unknown" << std::endl;
+            _scroll.movement = _event.mouseWheelScroll.delta;
+            _mousePos = {_event.mouseWheelScroll.x, _event.mouseWheelScroll.y};
 
-            case sf::Event::MouseEntered:
-                _mouseInWindow = true;
+        /** Mouse Moved **/
+        case sf::Event::MouseMoved:
+            _mousePos = {_event.mouseWheelScroll.x, _event.mouseWheelScroll.y};
 
-            case sf::Event::MouseLeft:
-                _mouseInWindow = false;
+        /** Mouse Entered In Window **/
+        case sf::Event::MouseEntered:
+            _mouseInWindow = true;
 
-            default:
-                break;
-        }
+        /** Mouse Left The Window **/
+        case sf::Event::MouseLeft:
+            _mouseInWindow = false;
+
+        default:
+            break;
     }
 }
