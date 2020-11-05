0f007908883445bd76ee95424e3ddd75b62b6dd3
Emilien Delevoye <emilien.delevoye@epitech.eu>
2020-11-05 14:56:30 +0100

    Add UDP class receiver for server

---
+++ server/client/UDP/UdpSocketInput.cpp
@@ -0,0 +1,43 @@
+/*
+** EPITECH PROJECT, 2020
+** RType
+** File description:
+** Created by Emilien
+*/
+
+#include "UdpSocketInput.hpp"
+
+UdpSocketInput::UdpSocketInput(int port)
+{
+    _socketInput = udp::socket(_ioServiceInput);
+    _socketInput.open(udp::v4());
+    _receiveBuffer.clear();
+    _receiveBuffer.resize(240);
+    _socketInput.bind(udp::endpoint(address::from_string("0.0.0.0"), port));
+    _socketInput.async_receive_from(boost::asio::buffer(_receiveBuffer), _remoteEndpointInput,
+        boost::bind(&UdpSocketInput::_handleReceive, this,
+            boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
+    _threadReceiver = std::thread([&] { _ioServiceInput.run(); } );
+}
+
+std::vector<int> UdpSocketInput::getDataFromServer() const
+{
+    return _receiveBuffer;
+}
+
+void UdpSocketInput::_handleReceive(const boost::system::error_code &error, size_t bytes)
+{
+    if (error) {
+        std::cerr << "Receive failed: " << error.message() << std::endl;
+        return;
+    }
+    _socketInput.async_receive_from(boost::asio::buffer(_receiveBuffer), _remoteEndpointInput,
+        boost::bind(&UdpSocketInput::_handleReceive, this,
+            boost::asio::placeholders::error, boost::asio::placeholders::bytes_transferred));
+}
+
+UdpSocketInput::~UdpSocketInput()
+{
+    _ioServiceInput.stop();
+    _threadReceiver.join();
+}
