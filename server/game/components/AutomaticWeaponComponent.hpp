140cbc38dcbd8adf5730f9d8228af3b374b565b4
Cyprien Ricque <cyprien.ricque@epitech.eu>
2020-11-13 17:50:46 +0100

    Add automatic shot

--- server/game/components/AutomaticWeaponComponent.hpp
+++ server/game/components/AutomaticWeaponComponent.hpp
@@ -11,25 +11,24 @@
 class AutomaticWeaponComponent : public Engine::Component
 {
 private:
-    double _cooldown;
-    std::chrono::high_resolution_clock::time_point _lastShoot;
-    char _upgrade = 0;
-    char _maxUpgrade = 3;
+    double _cooldown = 2;
+    std::chrono::high_resolution_clock::time_point _lastShoot = std::chrono::high_resolution_clock::now();
+
     double _baseDamage;
     double _damageMultiplier;
+    double _shotSpeed = -30;
 
 public:
     explicit AutomaticWeaponComponent() : _baseDamage(0), _damageMultiplier(0), _cooldown(0),
     _lastShoot(std::chrono::high_resolution_clock::now()), Engine::Component() {}
-    explicit AutomaticWeaponComponent(double damage, double multiplier, double cooldown)
+    explicit AutomaticWeaponComponent(double damage, double multiplier, double cooldown, double shotSpeed)
     : _baseDamage(damage), _damageMultiplier(multiplier), _cooldown(cooldown), _lastShoot(std::chrono::high_resolution_clock::now())
-    , Engine::Component() {}
+    , _shotSpeed(shotSpeed), Engine::Component() {}
 
-    void upgrade() {if (this->_upgrade < this->_maxUpgrade) this->_upgrade++;}
-
-    [[nodiscard]] double getCurrentDamages() const {return (this->_baseDamage + (_baseDamage * _damageMultiplier * _upgrade));}
+    [[nodiscard]] double getCurrentDamages() const {return (this->_baseDamage + (_baseDamage * _damageMultiplier));}
     [[nodiscard]] bool canShoot() const {return Engine::Timer::hasElapsed(_lastShoot, _cooldown);}
     void refreshShoots() {_lastShoot = std::chrono::high_resolution_clock::now();}
+    [[nodiscard]] double getShotSpeed() {return _shotSpeed;}
 };
 
 #endif //RTYPE_AUTOMATICWEAPONCOMPONENT_HPP
